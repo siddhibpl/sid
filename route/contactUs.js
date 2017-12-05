eb9ecebfdc1270597bde528e85664433318aa9c7
siddhibpl <ambujdubey89@yahoo.com>
2017-12-05 12:08:24 +0530

    made config file

--- route/contactUs.js
+++ route/contactUs.js
@@ -2,38 +2,49 @@ var express = require('express');
 var router = express.Router();
 const nodemailer = require('nodemailer');
 const xoauth2 = require('xoauth2');
+var config = require('../config/config.js');
 
-
+var transporter = nodemailer.createTransport({
+  service: config.service,
+  auth: {
+    type: config.authentication.type,
+    user: config.authentication.user,
+    clientId: config.authentication.clientId,
+    clientSecret: config.authentication.clientSecret,
+    refreshToken: config.authentication.refreshToken,
+    accessToken: config.authentication.accessToken,
+  },
+})
+var maillist = [
+  config.maillist.admin1,
+  config.maillist.admin4,
+  config.maillist.admin3,
+];
 router.post('/contactUs', function(req, res, next) {
   console.log(req.body);
 
-var mailOptions = {
+  var mailOptions = {
     from: req.body["contactEmail"],
-    to: 'group.itijobs@gmail.com',
+    to: maillist,
     subject: req.body["contactSubject"],
-    html: '<html><head><title>Contact Us</title></head><body><p><b>Hi Sir/Madam,</b></p><p>This is ' + req.body["contactName"] + ', requesting <b>'+  req.body['contactText'] + '<b></p><p>Thanks & Regards,<br />' + req.body["contactName"] +'<p>Mobile-' + req.body["contactMobile"] +'</p><p>' + req.body["contactEmail"] +'</p><p><a href="http://localhost:8000">Redirect To Login</a></p>'+'</p></body></html>',
-    // text: 'Hi Sir/Madam<br>I am' + req.body['contactName'] +'requesting <b>'+ req.body['contactText'] +'</b><br>'
-};
-console.log("before");
-transporter.sendMail(mailOptions, function (err, res) {
-  if(error){
-         res.emailSent = false;
-  } else{
-         res.emailSent = true;
-  }
-  res.send(res);
-  // if (err) {
-  // console.log("After--err");
-  //   return next(err);
-  // } else {
-  // console.log("After--err");
-  // console.log(res);
-  //   return res.json({
-  //     "resCode": "OK",
-  //     "msg": "Mail Send Successfully!"
-  //   });
-  // }
-});
+    html: '<html><head><title>Contact Us</title></head><body><p><b>Hi Sir/Madam,</b></p><p>This is ' + req.body["contactName"] + ', requesting <b>' + req.body['contactText'] + '<b></p><p>Thanks & Regards,<br />' + req.body["contactName"] + '<p>Mobile-' + req.body["contactMobile"] + '</p><p>' + req.body["contactEmail"] + '</p><p><a href="http://localhost:8000">Redirect To Login</a></p>' + '</p></body></html>',
+  };
+  console.log("before");
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.json({
+        "resCode": 'Error',
+        "msg": "Error!"
+      });
+    } else {
+      console.log('Message sent: ' + info.response);
+      res.json({
+        "resCode": "OK",
+        "msg": "Mail Send Successfully!"
+      });
+    };
+  });
 });
 
 module.exports = router;
